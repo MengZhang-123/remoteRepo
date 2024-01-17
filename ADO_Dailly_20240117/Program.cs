8752a35adfdd529f15c5dcd6c301a3a921401f73
Meng Zhang <v-zhangmeng@microsoft.com>
2024-01-17 03:12:57 +0000

    Add project files.

---
+++ ADO_Dailly_20240117/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO_Dailly_20240117
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
